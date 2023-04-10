1d4d11e6e95023d52f2fd48ce0d05906e800b436
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-04-10 18:50:30 +0300

    add info user and enemy hp

--- js/index.js
+++ js/index.js
@@ -13,7 +13,24 @@ function handleSubmit(e) {
     email: e.target[1].value,
   };
   localStorage.setItem("form", JSON.stringify(data));
-  console.log(refs.body);
-  refs.body.style.backgroundImage = `url("../images/iapqmzd3zu721.jpg")`;
+
+  // показуємо секцію hero , змінюємо фон
+
+  refs.body.style.backgroundImage = `url("../images/background/iapqmzd3zu721.jpg")`;
   refs.heroSection.style.display = "flex";
+  console.log(refs.infoName);
+  refs.infoName.textContent = data.name;
+}
+
+refs.hero.addEventListener("click", handleClick);
+
+function handleClick() {
+  let countScore = +refs.infoScore.textContent;
+  refs.infoScore.textContent = countScore + 1;
+  let allClicks = +refs.infoAllClick.textContent;
+  refs.infoAllClick.textContent = allClicks + 1;
+  let hp = refs.enemyHp.offsetHeight;
+  const oneHit = 100 / 20;
+
+  refs.enemyHp.style.height = `${hp - oneHit}px`;
 }
--- js/refs.js
+++ js/refs.js
@@ -3,4 +3,9 @@ export const refs = {
   formSection: document.querySelector(".form-section"),
   form: document.querySelector(".form"),
   heroSection: document.querySelector(".hero-section"),
+  hero: document.querySelector(".hero-container"),
+  infoScore: document.querySelector("#info-score"),
+  infoAllClick: document.querySelector("#info-clicks"),
+  infoName: document.querySelector("#info-name"),
+  enemyHp: document.querySelector(".hero-enemy-hp"),
 };
