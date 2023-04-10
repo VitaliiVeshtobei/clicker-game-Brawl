e557ac470a82b56a3dd90f160fc9fd7b0fb0ddba
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-04-10 15:23:37 +0300

    add hero image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+import { refs } from "../js/refs.js";
 
+refs.form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  // при успішній заповненій формі скриваємо секцію з формою та записуємо дані в localstorage
+
+  refs.formSection.style.display = "none";
+  const data = {
+    name: e.target[0].value,
+    email: e.target[1].value,
+  };
+  localStorage.setItem("form", JSON.stringify(data));
+  console.log(refs.body);
+  refs.body.style.backgroundImage = `url("../images/iapqmzd3zu721.jpg")`;
+  refs.heroSection.style.display = "flex";
+}
---
+++ js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  body: document.querySelector("body"),
+  formSection: document.querySelector(".form-section"),
+  form: document.querySelector(".form"),
+  heroSection: document.querySelector(".hero-section"),
+};
